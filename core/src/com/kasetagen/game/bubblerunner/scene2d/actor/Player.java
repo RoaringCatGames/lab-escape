90060384b5e000174b90618adfdd57852b86bd43
Michael Revel <mjrevel@gmail.com>
2014-05-28 20:20:23 -0400

    Player based on KasetagenActor now

--- core/src/com/kasetagen/game/bubblerunner/scene2d/actor/Player.java
+++ core/src/com/kasetagen/game/bubblerunner/scene2d/actor/Player.java
@@ -1,6 +1,7 @@
 package com.kasetagen.game.bubblerunner.scene2d.actor;
 
-import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.kasetagen.engine.gdx.scenes.scene2d.KasetagenActor;
 
 /**
  * Created with IntelliJ IDEA.
@@ -9,5 +10,11 @@ import com.badlogic.gdx.scenes.scene2d.Actor;
  * Time: 7:43 PM
  * To change this template use File | Settings | File Templates.
  */
-public class Player extends Actor {
+public class Player extends KasetagenActor {
+
+	@Override
+	protected void drawFull(SpriteBatch batch, float parentAlpha) {
+		// TODO Auto-generated method stub
+		
+	}
 }
