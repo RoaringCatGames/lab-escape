3f14ec49315d3d2802844fc0bc831822dd3f16ee
Barryrowe <barryrowe@gmail.com>
2014-06-02 22:14:02 -0400

    Initial swapping out of plain figures for actors of forcefields

--- core/src/com/kasetagen/game/bubblerunner/scene2d/actor/Player.java
+++ core/src/com/kasetagen/game/bubblerunner/scene2d/actor/Player.java
@@ -4,6 +4,7 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.utils.Array;
 import com.kasetagen.game.bubblerunner.util.ForceFieldColorUtil;
 
@@ -16,7 +17,7 @@ import com.kasetagen.game.bubblerunner.util.ForceFieldColorUtil;
  * To change this template use File | Settings | File Templates.
  */
 
-public class Player extends GenericActor {
+public class Player extends GenericGroup {
 
     //set them in reverse order so they match with the order of the
     //  forcefields being added.
@@ -25,7 +26,8 @@ public class Player extends GenericActor {
 
 
     public int maxFields = 3;
-    private Array<ForceFieldType> fields;
+    //private Array<ForceFieldType> fields;
+    private Array<ForceField> fields;
 
     public Player(float x, float y, float width, float height){
         super(x, y, width, height, Color.BLACK);
@@ -34,36 +36,62 @@ public class Player extends GenericActor {
 
         //TODO: Replace ShapeRendering with Animation
         forceFieldType = ForceFieldType.BUBBLE;
-        fields = new Array<ForceFieldType>();
+        //fields = new Array<ForceFieldType>();
+        fields = new Array<ForceField>();
     }
 
     public void addField(ForceFieldType ff){
         if(fields.size == maxFields){
+            //Remove the forcefield
+            ForceField f = fields.get(0);
+            this.removeActor(f);
             fields.removeIndex(0);
         }
-        fields.add(ff);
+
+        float radius = getWidth()*(fields.size + 1);
+        ForceField field = new ForceField(getX(), getY(), radius, ff);
+
+        this.addActor(field);
+        fields.add(field);
     }
 
     public void addField(ForceFieldType ff, int index){
 
-        fields.insert(index, ff);
+        float radius = getWidth() * (index + 1);
+        ForceField field = new ForceField(getX(), getY(), radius, ff);
+
+        //Remove the overriding forcefield
+        if(fields.size > index){
+            ForceField f = fields.get(index);
+            if(f != null){
+                this.removeActor(f);
+            }
+        }
+        this.addActor(field);
+        fields.insert(index, field);
     }
 
     public void removeField(ForceFieldType ff){
         int removeIndex = -1;
         for(int i=0;i<fields.size;i++){
-            if(ff.equals(fields.get(i))){
+            if(ff.equals(fields.get(i).forceFieldType)){
                 removeIndex = i;
                 break;
             }
         }
 
         if(removeIndex >= 0){
+            ForceField f = fields.get(removeIndex);
+            this.removeActor(f);
             fields.removeIndex(removeIndex);
         }
     }
 
     public void clearFields(){
+
+        for(ForceField f:fields){
+            this.removeActor(f);
+        }
         fields.clear();
     }
 
@@ -74,7 +102,7 @@ public class Player extends GenericActor {
     public ForceFieldType getOuterForceField(){
         ForceFieldType ff = null;
         if(fields.size > 0){
-            ff = fields.get(0);
+            ff = fields.get(0).forceFieldType;
         }
 
         return ff;
@@ -90,27 +118,27 @@ public class Player extends GenericActor {
     }
 
     @Override
-    public void draw(Batch batch, float parentAlpha) {
-        super.draw(batch, parentAlpha);
+    public void drawFull(Batch batch, float parentAlpha) {
+//        super.draw(batch, parentAlpha);
 
-        batch.end();
-        batch.begin();
-        Gdx.gl20.glLineWidth(5f);
-        debugRenderer.setProjectionMatrix(getStage().getCamera().combined);
-        debugRenderer.begin(ShapeRenderer.ShapeType.Line);
-
-        //float radius = 160f;
-        for(int i = fields.size -1; i>=0;i--){
-            debugRenderer.setColor(getColor());
-            debugRenderer.setColor(ForceFieldColorUtil.getColor(fields.get(i)));
-            debugRenderer.circle(getOriginX(), getOriginY(), forceFieldColliders[i]);
-            debugRenderer.setColor(Color.WHITE);
-            debugRenderer.rect(getOuterForceFieldPosition(), getOuterForceFieldPosition() + 5f, 5f, 5f);
-            //radius += 20f;
-        }
-        debugRenderer.end();
-        batch.end();
-        batch.begin();
+//        batch.end();
+//        batch.begin();
+//        Gdx.gl20.glLineWidth(5f);
+//        debugRenderer.setProjectionMatrix(getStage().getCamera().combined);
+//        debugRenderer.begin(ShapeRenderer.ShapeType.Line);
+//
+//        //float radius = 160f;
+//        for(int i = fields.size -1; i>=0;i--){
+//            debugRenderer.setColor(getColor());
+//            debugRenderer.setColor(ForceFieldColorUtil.getColor(fields.get(i).forceFieldType));
+//            debugRenderer.circle(getOriginX(), getOriginY(), forceFieldColliders[i]);
+//            debugRenderer.setColor(Color.WHITE);
+//            debugRenderer.rect(getOuterForceFieldPosition(), getOuterForceFieldPosition() + 5f, 5f, 5f);
+//            //radius += 20f;
+//        }
+//        debugRenderer.end();
+//        batch.end();
+//        batch.begin();
 
     }
 }
