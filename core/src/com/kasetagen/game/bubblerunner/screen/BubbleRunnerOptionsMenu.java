5a6344e369b2e0c3860a4c37a1a8c8ebae8a0852
Barryrowe <barryrowe@gmail.com>
2014-08-10 00:10:44 -0400

    Centered and Labeled Options values
    Added table and layout settings to the options menu.

--- core/src/com/kasetagen/game/bubblerunner/screen/BubbleRunnerOptionsMenu.java
+++ core/src/com/kasetagen/game/bubblerunner/screen/BubbleRunnerOptionsMenu.java
@@ -5,9 +5,7 @@ import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
-import com.badlogic.gdx.scenes.scene2d.ui.Skin;
-import com.badlogic.gdx.scenes.scene2d.ui.Slider;
-import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.ui.*;
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.utils.viewport.ExtendViewport;
@@ -32,22 +30,38 @@ public class BubbleRunnerOptionsMenu extends BaseBubbleRunnerScreen{
     Slider sfxVolumeSet;
     TextButton backToMainMenuButton;
 
+    Label bgValue;
+    Label sfxValue;
+
+
+    private String getFloatStringVal(float val){
+        return Integer.toString((int)Math.floor(val * 10));
+    }
+
     private IDataSaver bgDataSaver, sfxDataSaver;
     public BubbleRunnerOptionsMenu(IGameProcessor delegate){
         super(delegate);
 
         stage = new BaseStage();
 
+        float sfxVolValue = delegate.getStoredFloat(GameOptions.SFX_MUSIC_VOLUME_PREF_KEY);
+        float bgVolValue = delegate.getStoredFloat(GameOptions.BG_MUSIC_VOLUME_PREF_KEY);
         Skin skin = gameProcessor.getAssetManager().get(AssetsUtil.DEFAULT_SKIN, AssetsUtil.SKIN);
 
-        bgVolumeSet = new Slider(0f, 1f, 0.1f, false, skin); //new Slider(0f, 1f, 0.1f, false, sliderStyle);
-        bgVolumeSet.setPosition(ViewportUtil.VP_WIDTH / 3, ViewportUtil.VP_HEIGHT*(5f/8f));
-        bgVolumeSet.setValue(delegate.getStoredFloat(GameOptions.BG_MUSIC_VOLUME_PREF_KEY));
+        Label bgVolLbl = new Label("Background Volume: " , skin);
+        bgValue = new Label(getFloatStringVal(bgVolValue), skin);
+        Label sfxVolLbl = new Label("SFX Volume: " , skin);
+        sfxValue = new Label(getFloatStringVal(sfxVolValue), skin);
+
+        bgVolumeSet = new Slider(0f, 1f, 0.1f, false, skin);
+        bgVolumeSet.setValue(bgVolValue);
 
         bgDataSaver = new IDataSaver() {
             @Override
             public void updatePreferences(Preferences prefs) {
                 prefs.putFloat(GameOptions.BG_MUSIC_VOLUME_PREF_KEY, bgVolumeSet.getValue());
+
+                bgValue.setText(getFloatStringVal(bgVolumeSet.getValue()));
             }
         };
 
@@ -58,18 +72,15 @@ public class BubbleRunnerOptionsMenu extends BaseBubbleRunnerScreen{
             }
         });
 
-        stage.addActor(bgVolumeSet);
 
-
-
-        sfxVolumeSet = new Slider(0f, 1f, 0.1f, false, skin); //new Slider(0f, 1f, 0.1f, false, sliderStyle);
-        sfxVolumeSet.setPosition(ViewportUtil.VP_WIDTH / 3, ViewportUtil.VP_HEIGHT*(3f/8f));
-        sfxVolumeSet.setValue(delegate.getStoredFloat(GameOptions.SFX_MUSIC_VOLUME_PREF_KEY));
+        sfxVolumeSet = new Slider(0f, 1f, 0.1f, false, skin);
+        sfxVolumeSet.setValue(sfxVolValue);
 
         sfxDataSaver = new IDataSaver() {
             @Override
             public void updatePreferences(Preferences prefs) {
                 prefs.putFloat(GameOptions.SFX_MUSIC_VOLUME_PREF_KEY, sfxVolumeSet.getValue());
+                sfxValue.setText(getFloatStringVal(sfxVolumeSet.getValue()));
             }
         };
 
@@ -79,8 +90,6 @@ public class BubbleRunnerOptionsMenu extends BaseBubbleRunnerScreen{
                 gameProcessor.saveGameData(sfxDataSaver);
             }
         });
-
-        stage.addActor(sfxVolumeSet);
 
 
         TextButton.TextButtonStyle style = new TextButton.TextButtonStyle();
@@ -98,10 +107,32 @@ public class BubbleRunnerOptionsMenu extends BaseBubbleRunnerScreen{
                 gameProcessor.changeToScreen(BubbleRunnerGame.MENU);
             }
         });
-        backToMainMenuButton.setPosition(((stage.getWidth()/4) * 3) - backToMainMenuButton.getWidth()/2, (stage.getHeight()/4));
 
-        stage.addActor(backToMainMenuButton);
+        int colWidth = 100;
+        Table table = new Table(skin);
+        table.setFillParent(true);
+        stage.addActor(table);
 
+        table.add().expandX();
+        table.add(bgVolLbl).width(colWidth);
+        table.add(bgVolumeSet).width(colWidth);
+        table.add(bgValue).width(colWidth);
+        table.add().expandX();
+        table.row();
+
+        table.add().expandX();
+        table.add(sfxVolLbl).width(colWidth);
+        table.add(sfxVolumeSet).width(colWidth);
+        table.add(sfxValue).width(colWidth);
+        table.add().expandX();
+
+        table.row();
+        table.add().expandX();
+        table.add().expandX();
+        table.add(backToMainMenuButton);
+        table.add().expandX();
+        table.add().expandX();
+        table.debug();
     }
 
     @Override
