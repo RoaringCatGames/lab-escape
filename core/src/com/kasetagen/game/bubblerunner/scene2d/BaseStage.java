accbbeb3659f292b81261b9015e7461ba821c0de
Barryrowe <barryrowe@gmail.com>
2015-10-01 23:22:46 -0400

    Apply fit viewport
    Fit viewport is now in place.

--- core/src/com/kasetagen/game/bubblerunner/scene2d/BaseStage.java
+++ core/src/com/kasetagen/game/bubblerunner/scene2d/BaseStage.java
@@ -1,5 +1,7 @@
 package com.kasetagen.game.bubblerunner.scene2d;
 
+import com.badlogic.gdx.utils.viewport.FillViewport;
+import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.StretchViewport;
 import com.kasetagen.engine.IGameProcessor;
 import com.kasetagen.engine.gdx.scenes.scene2d.Kitten2dStage;
@@ -14,7 +16,7 @@ import com.kasetagen.game.bubblerunner.util.ViewportUtil;
 public class BaseStage extends Kitten2dStage {
 
     public BaseStage(IGameProcessor gp){
-        super(new StretchViewport(ViewportUtil.VP_WIDTH, ViewportUtil.VP_HEIGHT), gp);
+        super(new FitViewport(ViewportUtil.VP_WIDTH, ViewportUtil.VP_HEIGHT), gp);
     }
 
 }
